5ab27d22e384ab7a2a6dfe1132d9f25034f9247a
Javan Makhmali <javan@javan.us>
2017-07-31 13:11:44 -0400

    Freshen JavaScript / Google API usage

--- lib/google_sign_in/helper.rb
+++ lib/google_sign_in/helper.rb
@@ -14,7 +14,9 @@ module GoogleSignIn
 
     private
       def google_sign_in_javacript_include_tag
-        javascript_include_tag "https://apis.google.com/js/platform.js?onload=setupGoogleSignIn", async: true, defer: true,
+        javascript_include_tag "https://apis.google.com/js/api.js", async: true, defer: true,
+          onload: "this.onload=function(){};setupGoogleSignIn()",
+          onreadystatechange: "if (this.readyState === 'complete') this.onload()",
           data: { turbolinks_track: :reload, force_turbolinks_reload: Time.now.to_i }
       end
 
@@ -29,33 +31,38 @@ module GoogleSignIn
       end
 
       def google_sign_in_click_handler(&block)
-        tag.div(onclick: 'selectGoogleSignInAccount(event)') { capture(&block) }
+        tag.div(id: "google_signin_container") { capture(&block) }
       end
 
       def google_sign_in_javascript_tag
-        javascript_tag(<<-EOS
-          function setupGoogleSignIn() {
-            gapi.load("auth2", function () { gapi.auth2.init() })
-          }
+        javascript_tag <<-JS.strip_heredoc
+          (function() {
+            function installAuthClient(callback) {
+              gapi.load("client:auth2", function() {
+                gapi.auth2.init().then(callback)
+              })
+            }
 
-          function selectGoogleSignInAccount(event) {
-            event.preventDefault()
+            function installClickHandler() {
+              var options = new gapi.auth2.SigninOptionsBuilder()
+              options.setPrompt("select_account")
+              gapi.auth2.getAuthInstance().attachClickHandler("google_signin_container", options, handleSignIn)
+            }
 
-            options = new gapi.auth2.SigninOptionsBuilder
-            options.setPrompt("select_account")
-
-            gapi.auth2.getAuthInstance().signIn(options).then(function (googleUser) {
+            function handleSignIn(googleUser) {
               var token = googleUser.getAuthResponse().id_token
-
-              if (token !== null) {
-                document.getElementById('google_id_token').value = token
-                document.getElementById('google_signin').submit()
+              if (token) {
+                document.getElementById("google_id_token").value = token
+                document.getElementById("google_signin").submit()
                 gapi.auth2.getAuthInstance().signOut()
               }
-            })
-          }
-  EOS
-        )
+            }
+
+            window.setupGoogleSignIn = function() {
+              installAuthClient(installClickHandler)
+            }
+          })()
+        JS
       end
   end
 end
