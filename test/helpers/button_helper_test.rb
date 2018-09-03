f10bfe5c78e04136c8355bd4c6cc798e17aab44d
George Claghorn <george@basecamp.com>
2018-09-02 23:20:46 -0400

    Test generating a login button with custom attributes

--- test/helpers/button_helper_test.rb
+++ test/helpers/button_helper_test.rb
@@ -1,8 +1,8 @@
-require 'test_helper'
+require "test_helper"
 
 class GoogleSignIn::ButtonHelperTest < ActionView::TestCase
   test "generating a login button with text content" do
-    assert_dom_equal <<-HTML, google_sign_in_button("Log in with Google", proceed_to: 'https://www.example.com/login')
+    assert_dom_equal <<-HTML, google_sign_in_button("Log in with Google", proceed_to: "https://www.example.com/login")
       <form action="/google_sign_in/authorization" accept-charset="UTF-8" data-remote="true" method="post">
         <input name="utf8" type="hidden" value="&#x2713;" />
         <input name="proceed_to" type="hidden" value="https://www.example.com/login" />
@@ -12,11 +12,24 @@ class GoogleSignIn::ButtonHelperTest < ActionView::TestCase
   end
 
   test "generating a login button with HTML content" do
-    assert_dom_equal <<-HTML, google_sign_in_button(proceed_to: 'https://www.example.com/login') { image_tag('google.png') }
+    assert_dom_equal <<-HTML, google_sign_in_button(proceed_to: "https://www.example.com/login") { image_tag("google.png") }
       <form action="/google_sign_in/authorization" accept-charset="UTF-8" data-remote="true" method="post">
         <input name="utf8" type="hidden" value="&#x2713;" />
         <input name="proceed_to" type="hidden" value="https://www.example.com/login" />
         <button type="submit"><img src="/images/google.png"></button>
+      </form>
+    HTML
+  end
+
+  test "generating a login button with custom attributes" do
+    button = google_sign_in_button("Log in with Google", proceed_to: "https://www.example.com/login",
+      class: "login-button", data: { disable_with: "Loading Google login…" })
+
+    assert_dom_equal <<-HTML, button
+      <form action="/google_sign_in/authorization" accept-charset="UTF-8" data-remote="true" method="post">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <input name="proceed_to" type="hidden" value="https://www.example.com/login" />
+        <button type="submit" class="login-button" data-disable-with="Loading Google login…">Log in with Google</button>
       </form>
     HTML
   end
