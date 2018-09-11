5338fbee74047120d9a56752be3803eaf579c872
George Claghorn <george@basecamp.com>
2018-09-11 09:49:37 -0400

    Generate a local form
    Without Turbolinks, Rails UJS turns cross-origin HTTP redirects into illegal XHRs. Fixes #14.

--- app/helpers/google_sign_in/button_helper.rb
+++ app/helpers/google_sign_in/button_helper.rb
@@ -1,6 +1,6 @@
 module GoogleSignIn::ButtonHelper
   def google_sign_in_button(text = nil, proceed_to:, **options, &block)
-    form_with url: google_sign_in.authorization_path do
+    form_with url: google_sign_in.authorization_path, local: true do
       hidden_field_tag(:proceed_to, proceed_to, id: nil) + button_tag(text, name: nil, **options, &block)
     end
   end
--- test/helpers/button_helper_test.rb
+++ test/helpers/button_helper_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class GoogleSignIn::ButtonHelperTest < ActionView::TestCase
   test "generating a login button with text content" do
     assert_dom_equal <<-HTML, google_sign_in_button("Log in with Google", proceed_to: "https://www.example.com/login")
-      <form action="/google_sign_in/authorization" accept-charset="UTF-8" data-remote="true" method="post">
+      <form action="/google_sign_in/authorization" accept-charset="UTF-8" method="post">
         <input name="utf8" type="hidden" value="&#x2713;" />
         <input name="proceed_to" type="hidden" value="https://www.example.com/login" />
         <button type="submit">Log in with Google</button>
@@ -13,7 +13,7 @@ class GoogleSignIn::ButtonHelperTest < ActionView::TestCase
 
   test "generating a login button with HTML content" do
     assert_dom_equal <<-HTML, google_sign_in_button(proceed_to: "https://www.example.com/login") { image_tag("google.png") }
-      <form action="/google_sign_in/authorization" accept-charset="UTF-8" data-remote="true" method="post">
+      <form action="/google_sign_in/authorization" accept-charset="UTF-8" method="post">
         <input name="utf8" type="hidden" value="&#x2713;" />
         <input name="proceed_to" type="hidden" value="https://www.example.com/login" />
         <button type="submit"><img src="/images/google.png"></button>
@@ -26,7 +26,7 @@ class GoogleSignIn::ButtonHelperTest < ActionView::TestCase
       class: "login-button", data: { disable_with: "Loading Google login…" })
 
     assert_dom_equal <<-HTML, button
-      <form action="/google_sign_in/authorization" accept-charset="UTF-8" data-remote="true" method="post">
+      <form action="/google_sign_in/authorization" accept-charset="UTF-8" method="post">
         <input name="utf8" type="hidden" value="&#x2713;" />
         <input name="proceed_to" type="hidden" value="https://www.example.com/login" />
         <button type="submit" class="login-button" data-disable-with="Loading Google login…">Log in with Google</button>
