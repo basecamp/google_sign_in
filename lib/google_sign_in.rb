393e4364e45a75bea450ebfbecbbd56900f09744
Jeremy Daer <jeremydaer@gmail.com>
2019-03-05 22:10:41 -0800

    Pass OAuth2 access token request errors through to the app
    Rescue unhandled OAuth2::Error for access token requests and pass to the
    app just like authorization request errors.

--- lib/google_sign_in.rb
+++ lib/google_sign_in.rb
@@ -5,8 +5,8 @@ module GoogleSignIn
   mattr_accessor :client_id
   mattr_accessor :client_secret
 
-  # Authorization Code Grant errors: https://tools.ietf.org/html/rfc6749#section-4.1.2.1
-  OAUTH2_ERRORS = %w[
+  # https://tools.ietf.org/html/rfc6749#section-4.1.2.1
+  authorization_request_errors = %w[
     invalid_request
     unauthorized_client
     access_denied
@@ -15,6 +15,20 @@ module GoogleSignIn
     server_error
     temporarily_unavailable
   ]
+
+  # https://tools.ietf.org/html/rfc6749#section-5.2
+  access_token_request_errors = %w[
+    invalid_request
+    invalid_client
+    invalid_grant
+    unauthorized_client
+    unsupported_grant_type
+    invalid_scope
+  ]
+
+  # Authorization Code Grant errors from both authorization requests
+  # and access token requests.
+  OAUTH2_ERRORS = authorization_request_errors | access_token_request_errors
 end
 
 require 'google_sign_in/identity'
