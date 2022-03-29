c4c34f08ec764538db3c2f2270a2a0be287e068c
Rodrigo Urubatan <rodrigo.jardim@brightwire.com>
2022-03-29 09:15:49 -0300

    send wither access token or refresh token, if the APP wants a refresh token it can get an access token from google and if they do not we send the access token, this change is implemented to prevent CookieOverflow

--- app/controllers/google_sign_in/callbacks_controller.rb
+++ app/controllers/google_sign_in/callbacks_controller.rb
@@ -3,48 +3,51 @@ require 'google_sign_in/redirect_protector'
 class GoogleSignIn::CallbacksController < GoogleSignIn::BaseController
   def show
     redirect_to proceed_to_url, flash: { google_sign_in: google_sign_in_response }
-  rescue GoogleSignIn::RedirectProtector::Violation => error
-    logger.error error.message
+  rescue GoogleSignIn::RedirectProtector::Violation => e
+    logger.error e.message
     head :bad_request
   end
 
   private
-    def proceed_to_url
-      flash[:proceed_to].tap { |url| GoogleSignIn::RedirectProtector.ensure_same_origin(url, request.url) }
-    end
 
-    def google_sign_in_response
-      if valid_request? && params[:code].present?
-        { id_token: id_token, access_token: access_token, refresh_token: refresh_token }
-      else
-        { error: error_message_for(params[:error]) }
-      end
-    rescue OAuth2::Error => error
-      { error: error_message_for(error.code) }
-    end
+  def proceed_to_url
+    flash[:proceed_to].tap { |url| GoogleSignIn::RedirectProtector.ensure_same_origin(url, request.url) }
+  end
 
-    def valid_request?
-      flash[:state].present? && params[:state] == flash[:state]
+  def google_sign_in_response
+    if valid_request? && params[:code].present?
+      response = { id_token: id_token }
+      response[:refresh_token] = refresh_token if refresh_token
+      response[:access_token] = access_token unless refresh_token
+      response
+    else
+      { error: error_message_for(params[:error]) }
     end
+  rescue OAuth2::Error => e
+    { error: error_message_for(e.code) }
+  end
 
+  def valid_request?
+    flash[:state].present? && params[:state] == flash[:state]
+  end
 
-    def token
-      @token ||= client.auth_code.get_token(params[:code])
-    end
+  def token
+    @token ||= client.auth_code.get_token(params[:code])
+  end
 
-    def id_token
-      token['id_token']
-    end
+  def id_token
+    token['id_token']
+  end
 
-    def access_token
-      token.token
-    end
+  def access_token
+    token.token
+  end
 
-    def refresh_token
-      token.refresh_token
-    end
+  def refresh_token
+    token.refresh_token
+  end
 
-    def error_message_for(error_code)
-      error_code.presence_in(GoogleSignIn::OAUTH2_ERRORS) || "invalid_request"
-    end
+  def error_message_for(error_code)
+    error_code.presence_in(GoogleSignIn::OAUTH2_ERRORS) || 'invalid_request'
+  end
 end
