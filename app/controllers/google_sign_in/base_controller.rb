393d682b06873a1fa42118333d56c3e16a384568
Jeremy Daer <jeremy@37signals.com>
2022-10-27 15:19:19 -0700

    Configurable authorize_url so we can pass disallow_webview=true
    * https://developers.googleblog.com/2021/06/upcoming-security-changes-to-googles-oauth-2.0-authorization-endpoint.html#test
    * Tests against oauth2 2.0 and fixes up compatibility

--- app/controllers/google_sign_in/base_controller.rb
+++ app/controllers/google_sign_in/base_controller.rb
@@ -1,15 +1,8 @@
-require 'oauth2'
-
 class GoogleSignIn::BaseController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
     def client
-      @client ||= OAuth2::Client.new \
-        GoogleSignIn.client_id,
-        GoogleSignIn.client_secret,
-        authorize_url: 'https://accounts.google.com/o/oauth2/auth',
-        token_url: 'https://oauth2.googleapis.com/token',
-        redirect_uri: callback_url
+      @client ||= GoogleSignIn.oauth2_client(redirect_uri: callback_url)
     end
 end
--- test/dummy/config/initializers/google_sign_in.rb
+++ test/dummy/config/initializers/google_sign_in.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
   config.google_sign_in.client_id = FAKE_GOOGLE_CLIENT_ID
   config.google_sign_in.client_secret = FAKE_GOOGLE_CLIENT_SECRET
+
+  # Default changed to basic auth. Use old :request_body for the sake of our test stubs.
+  config.google_sign_in.oauth2_client_options = { auth_scheme: :request_body }
 end
