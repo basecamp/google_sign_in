0463bc5853142a2b0a11d62067d93584d9371e4b
Vlad Jebelev <vlad@jebelev.com>
2024-07-01 18:49:16 -0400

    Added custom redirect uri

--- app/controllers/google_sign_in/base_controller.rb
+++ app/controllers/google_sign_in/base_controller.rb
@@ -3,6 +3,14 @@ class GoogleSignIn::BaseController < ActionController::Base
 
   private
     def client
-      @client ||= GoogleSignIn.oauth2_client(redirect_uri: callback_url)
+      @client ||= GoogleSignIn.oauth2_client(redirect_uri: callback_url(host: redirect_host_only || redirect_host))
+    end
+
+    def redirect_host_only
+      Rails.application.config.action_mailer.default_url_options[:host_only]
+    end
+
+    def redirect_host
+      Rails.application.config.action_mailer.default_url_options[:host]
     end
 end
